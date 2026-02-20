5b3a02a02e1566947eddac833ad27e8938173f96
Bora Lee <dltngus1317@gmail.com>
2026-02-20 11:58:52 +0900

    chore:배포 환경에 맞춰 수정

--- next.config.js
+++ next.config.js
@@ -2,14 +2,12 @@
 const path = require('path')
 
 module.exports = {
-  output: 'export',
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@use "src/styles/utils.scss" as *;`,
   },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         hostname: '**',
